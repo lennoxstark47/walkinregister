4b2b8514911b589cd6fbbd092c7c2fa337dec067
lennoxstark47 <twisamhaldar@gmail.com>
2021-11-06 15:14:42 +0530

    added nice toggle switch

--- src/components/Editcx.jsx
+++ src/components/Editcx.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import Switch from '@mui/material/Switch';
 
 export default class Editcx extends Component {
 	constructor(props) {
@@ -19,6 +20,7 @@ export default class Editcx extends Component {
 			)
 			.then((res) => {
 				this.setState({
+					remarks: res.data.remarks,
 					isConverted: res.data.isConverted,
 					name: res.data.name,
 				});
@@ -107,6 +109,7 @@ export default class Editcx extends Component {
 									Remarks
 								</h6>
 								<textarea
+									value={this.state.remarks}
 									className='form-control'
 									aria-label='Remarks'
 									onChange={this.onRemarksChange}
@@ -120,17 +123,23 @@ export default class Editcx extends Component {
 									alignItems: 'center',
 									marginTop: '5px',
 								}}>
-								<div
-									style={{ marginLeft: '5px' }}>
-									<button
+								<div style={{ marginLeft: '5px' }}>
+									{/* <button
 										type='button'
 										className='btn btn-primary'
 										onClick={this.onToggle}>
 										Convert
-									</button>
+									</button> */}
+									<span>Convert</span>
+									<Switch
+										checked={this.state.isConverted}
+										onChange={this.onToggle}
+										inputProps={{
+											'aria-label': 'controlled',
+										}}
+									/>
 								</div>
-								<div
-									style={{ marginLeft: '10px' }}>
+								<div style={{ marginLeft: '10px' }}>
 									<button
 										type='button'
 										className='btn btn-primary'
